1d594afc65e1ae673e58d29d6519ad698f3306fb
nadezhdayu <nadezhdayureva1987@gmail.com>
2022-06-26 20:38:19 +0300

    Решение задачи 2

---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+void PrintArray(int[] r)
+{
+    for (int i = 0; i < r.Length; i++)
+    {
+        System.Console.Write(r[i] + " ");
+    }
+}
+int[] array;
+array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 100);
+
+}
+int SumOfOddPos(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i = i + 2)
+        result = result + array[i];
+    return result;
+}
+
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях равна {SumOfOddPos(array)}");
